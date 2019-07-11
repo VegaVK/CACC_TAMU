5e194151b22595b11bb63a47c01c599e92a755ab
Rangeesh <8917871+Rangeesh@users.noreply.github.com>
2019-07-10 22:53:25 -0500

    Added an exponential filter to the Velocity output

--- ll_controller/scripts/ACC_v2.py
+++ ll_controller/scripts/ACC_v2.py
@@ -21,6 +21,7 @@ class ACC:
         self.t=input("Enter Time Headway") # Time Headway when the car is Running
         self.relvel=0 # Relative Velocity of the Ego car ahead 
         self.speed=0 # Speed of the Autonomous Car
+        self.alpha=0 # Exponential Filter Hyperparameter
         self.flag=0
         self.time=0
         self.interr=0
@@ -29,14 +30,17 @@ class ACC:
         # Node Initialisation
         rospy.init_node('ACC',anonymous=True)
         # ROS Publishers
-        self.velpub=rospy.Publisher('/desired_velocity',Float32,queue_size=1) # Publishing Desired Velocity of the Car to the Controller 
+
+
+        #! Remember to change this to /speed_limit
+        self.velpub=rospy.Publisher('/speed_limit',Float32,queue_size=1) # Publishing Desired Velocity of the Car to the Controller 
         # ROS Subscribers
         self.AheadCarData=rospy.Subscriber('/AheadCarData',aheadcar,self.ACDCallback)  # Parameters of the Ego Vehicle
         self.speedsub=rospy.Subscriber('/autodrive_sim/output/speed', Float32, self.speedCallback) # Longitudinal Velocity of the Car
  
     
     def ACDCallback(self,msg):
-        self.cd=msg.RedDist
+        self.cd=msg.RelDist
         self.relvel=msg.RelVel_Radial
         self.VelocityPub()
 
@@ -44,8 +48,9 @@ class ACC:
         self.speed=msg.data
 
     def VelocityPub(self):
-        Kp=10
-        Kv=0.5
+        Kp=3
+        Kv=0.1
+        V=0
         self.td=max(self.t*self.speed, self.L)
         e=self.td-self.cd
         if self.flag==0:
@@ -56,31 +61,24 @@ class ACC:
             self.interr=self.interr + e*(rospy.get_time()-self.time)
             #V=Kp*e+Kv*self.interr
             Accel=-Kv*(self.relvel) - Kp*(self.cd + self.L + self.t*self.speed)
-            V=self.speed+Accel*(rospy.get_time()-self.time)
+            V1=self.speed+Accel*(rospy.get_time()-self.time)
+            V = V*self.alpha + V1*(1-self.alpha)
             self.time=rospy.get_time()
         self.velpub.publish(V)
-        rospy.spin()
+        print(V)
+        
 
 
 
 if __name__=="__main__":
     ACC()
-    
+    rospy.spin()
 
 '''
-
 So, what's the relative ditance between the cars??
 # Let us take that has an input
 Is that the only input?? No, but let us proceed with that for now
-
 So, we have the following targets
-
 1. Relative Distance between the car has to be fixed
 2. Relative Velocity must be 0 -- This target is automoatically achieved with the first one
-
-
-
-
-
-
 '''
